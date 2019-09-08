2a4a93a28616140de1c3deab30f261674efc8f7c
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-09-08 18:49:47 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
-
+     
+  return_hash = {
+    :known_for => programmer_hash[:grace_hopper][:known_for],
+    :languages => programmer_hash[:grace_hopper][:languages]
+  }
 
 end
 
@@ -37,6 +41,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  return_key = programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +61,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  return_key = programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,8 +86,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }
+  programmer_hash
 end
 
 def changing_alan
@@ -101,7 +111,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -121,5 +132,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
